7b6023be1e4c8b5ab54fde78d0d8f9ee84d464df
NAchref <achref.nefzaoui@esprit.tn>
2024-01-12 21:57:13 +0100

    Flask app

--- Flask-app-Devops/app.py
+++ Flask-app-Devops/app.py
@@ -1,9 +1,8 @@
 from dotenv import load_dotenv
 from flask import Flask, render_template
 import os
-
 from flask_sqlalchemy import SQLAlchemy
-import psycopg2
+import pymysql
 
 # Load environment variables
 load_dotenv()
@@ -22,22 +21,21 @@ db = SQLAlchemy()
 db.init_app(app)
 
 
-
-# Function to establish a database connection
-def get_database_connection():
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-        return conn
-    except psycopg2.Error as e:
-        print(f"Error connecting to the database: {e}")
-        return None 
-
-
-
+# Database connection info with environment variables
+db_connection = pymysql.connect(
+    host=os.environ.get('DB_HOST', 'localhost'),
+    user=os.environ.get('DB_USER', 'root'),
+    password=os.environ.get('DB_PASSWORD', 'password'),
+    database=os.environ.get('DB_DATABASE', 'todo_db'),
+    cursorclass=pymysql.cursors.DictCursor
+)
 
 
 # Pages Routes 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM tasks")
+    tasks = cursor.fetchall()
+    return render_template('index.html', tasks=tasks)
 
