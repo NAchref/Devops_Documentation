d7e408de91f5da895ccfc96ae0c9da71ceaf64dc
NAchref <achref.nefzaoui@esprit.tn>
2024-01-14 21:47:13 +0100

    Flask app

--- Flask-app-Devops/app.py
+++ Flask-app-Devops/app.py
@@ -1,8 +1,13 @@
+from dotenv import load_dotenv
 from flask import Flask, render_template
 import os
 
+# Load environment variables
+load_dotenv()
+
 app = Flask(__name__)
 
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 # Pages Routes 
 @app.route('/')
