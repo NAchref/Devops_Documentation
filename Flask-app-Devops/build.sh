d009424e5895f7259383399ad10a7b0dcc16db8d
NAchref <achref.nefzaoui@esprit.tn>
2024-01-18 23:17:13 +0100

    flaskapp

--- Flask-app-Devops/build.sh
+++ Flask-app-Devops/build.sh
@@ -4,4 +4,12 @@
 cluster_name="cluster-1-test"
 region="REGION"
 aws_id="AWS_ID"
-cd terraform 
+cd terraform
+app_img=$(terraform output -raw ecr_app_repository_name) 
+db_img=$(terraform output -raw ecr_db_repository_name)
+rds_endpoint=$(terraform output -raw rds_cluster_endpoint)
+db_username=$(terraform output -raw db_username)
+db_password=$(terraform output -raw db_password)
+app_image_name="$aws_id.dkr.ecr.$region.amazonaws.com/$app_img:latest"
+db_image_name="$aws_id.dkr.ecr.$region.amazonaws.com/$db_img:latest"
+cd .. 
