15bfcb574c8d928c0d9e9922fa152ba249c4e6d9
NAchref <achref.nefzaoui@esprit.tn>
2024-02-05 23:17:13 +0100

    tfrds secret manager

--- Flask-app-Devops/terraform/rds-sm.tf
+++ Flask-app-Devops/terraform/rds-sm.tf
@@ -5,3 +5,20 @@ resource "random_password" "password" {
   special          = true
   override_special = "_%@"
 }
+
+resource "aws_secretsmanager_secret" "rds_credentials" {
+  name        = "${var.rds_cluster_name}-secrets-4"
+  description = "RDS DB credentials"
+}
+
+
+
+# Set the values of the secrets in the Secret manager
+
+resource "aws_secretsmanager_secret_version" "rds_credentials_version" {
+  secret_id = aws_secretsmanager_secret.rds_credentials.id
+  secret_string = jsonencode({
+    "username" : var.db_username,
+    "password" : random_password.password.result
+  })
+}
